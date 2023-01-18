033445ae5b4b9f4e9dd415e06218ef458e4b90d7
Amaal Ali <amaalalikhadija@gmail.com>
2023-01-18 18:02:03 -0500

    worked on this paired programming with another group becuase i didnt hear back from my partner but I had the pleasure of working with Maryan and Asha

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,29 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  //console.log("horizontal", horizontalJoin);
+  for (let element of horizontalJoin) {
+    if (element.includes(word))
+      return true;
+  }
 
-module.exports = wordSearch
+  for (let j = 0; j < letters.length; j++) {
+    const verticalArray = [];
+    //console.log("horizontal array:", horizontalArray);
+    //console.log("letters[j]", letters[j]);
+    for (let i = 0; i < letters.length; i++) {
+      //console.log("letters[j][i]", letters[i][j])
+      if (letters[i][j]) {
+        verticalArray.push(letters[i][j]);
+      }
+    }
+    const lettersVertical = verticalArray.join('');
+    if (lettersVertical.includes(word)) {
+      return true;
+    }
+    //console.log("vertical Array", lettersVertical);
+  }
+  //If the loop closed and True was not not return;
+  return false; //It means the element was not found in the Array
+};
+
+
